5e2b6833ebdf5139f508a02a55742bcc12961da8
Edward Bramanti <edward@bramanti.org>
2018-03-23 12:08:47 -0700

    fix(react): Remove unnecessary React rules

--- react/index.js
+++ react/index.js
@@ -4,6 +4,10 @@ module.exports = {
         require.resolve("tslint-react")
     ],
     rules: {
-        "export-name": false
+        "export-name": false,
+        "no-this-assignment": false,
+        "jsx-no-multiline-js": false,
+        "jsx-boolean-value": false,
+        "react-this-binding-issue": false
     }
 }
